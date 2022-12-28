303b256b0b588636624ea178d720972ea4f5f4da
RafaelMunizz <rafaelvictormuniz@gmail.com>
2022-12-28 15:14:36 -0300

    Fim seção 9: Módulo de usuários

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,6 @@ import { UsersRepository } from './repositories/users.repository';
 
 @Injectable()
 export class UsersService {
-
   constructor(private readonly repository: UsersRepository) {}
 
   create(createUserDto: CreateUserDto) {
@@ -13,18 +12,18 @@ export class UsersService {
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.repository.findAll();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.repository.findOne(id);
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+    return this.repository.update(id, updateUserDto);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} user`;
+    return this.repository.remove(id);
   }
 }
